abd4b23e8d12592d902bcb7c6a0bcda4c08d84fd
Hongshun Yang <yanghongshun@gmail.com>
2016-10-30 23:53:41 -0400

    Initial commit

---
+++ tools/volcano.plot.r
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+    stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} 
+res <- read.table(args[1], header=TRUE)
+head(res)
+pdf(paste(args[1],'.pdf',sep=''),width=6,height=4,paper='special')
+with(res,plot(-log2(Coverage),Frequency,pch=19,cex=0.3,main=args[1],cex.main=0.5))
+with(subset(res, repFamilySINE=='Alu' ), points(-log2(Coverage),Frequency, pch=19,cex=0.3,col="red"))
+dev.off()
+
