4a63582baa61bf4032a06cd6bc4e81d8d8849f1a
Hongshun Yang <yanghongshun@gmail.com>
2016-11-01 00:25:51 -0400

    comment sth

--- tools/volcano.plot.r
+++ tools/volcano.plot.r
@@ -18,7 +18,10 @@ for (fl in fls){
     head(res)
     pdf(paste('_result/',basename(fl),'.pdf',sep=''),width=60,height=40)
     par(mai=c(5,5,5,5))
-    with(res,plot(-log10(Coverage),sqrt(Frequency),pch=19,cex=2,main=basename(fl),cex.main=4,cex.axis=2.5,cex.lab=4))
-    with(subset(res, repFamilySINE=='Alu' ), points(-log10(Coverage),sqrt(Frequency), pch=19,cex=2,col="red"))
+    #with(res,plot(-log10(Coverage),sqrt(Frequency),pch=19,cex=2,main=basename(fl),cex.main=4,cex.axis=2.5,cex.lab=4))
+    #with(subset(res, repFamilySINE=='Alu' ), points(-log10(Coverage),sqrt(Frequency), pch=19,cex=2,col="red"))
+
+    with(res,plot(-log10(Frequency),log2(Coverage),pch=19,cex=2,main=basename(fl),cex.main=4,cex.axis=2.5,cex.lab=4))
+    with(subset(res, repFamilySINE=='Alu' ), points(-log10(Frequency),log2(Coverage), pch=19,cex=2,col="red"))
     dev.off()
 }
