a658a2a7b2a5bc64e8183397fe5b0cdaeb32bcdc
Hongshun Yang <yanghongshun@gmail.com>
2016-11-01 16:50:56 -0400

    python style :library(optparse)

--- tools/plot.r
+++ tools/plot.r
@@ -1,47 +1,82 @@
 #!/usr/bin/env Rscript
 
 # support batch 
-#./volcano.plot.r ../result/10262016/
+#./plot.r -i ../result/10262016/ -m  fc,nlog10csqrtf,nlog10flog2c -c repFamilySINE==\'Alu\'
 # _result
 
-args = commandArgs(trailingOnly=TRUE)
-# test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-    stop("At least one argument must be supplied (input file).n", call.=FALSE)
+library("optparse");
+option_list = list(
+    make_option(
+        c('-i',"--input"),
+        type = "character",        
+        default = NULL,
+        help ="input directory",        
+        metavar = "character"         
+    ),               
+    make_option(
+        c('-m',"--mode"),
+        type = "character",        
+        default = 'fc',
+        help ="plot model data",        
+        metavar = "character"         
+    ),
+    make_option(
+        c('-c',"--condition"),
+        type = "character",        
+        default = "repFamilySINE==\'Alu\'",
+        help ="subset condition",        
+        metavar = "character"         
+    )
+);
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser)
+
+if (is.null(opt$input)) {
+    print_help(opt_parser)
+    stop("at least one argument must be supplied (input directory).n", call.=FALSE);
 }
 
 # args[1] directory
-fls <- list.files(args[1],pattern='*.csv',full.names=TRUE,recursive=TRUE)
-# args[2] FC,-log10CsqrtF,-log10Flog2C
-styl<-tolower(args[2])
+fls = list.files(opt$input,pattern='*.csv',full.names=TRUE,recursive=TRUE);
+mod = tolower(opt$mode);
+
+img_main = ""
+img_xlab = ""
+img_ylab = ""
 
 for (fl in fls){
-    res <- read.table(fl, header=TRUE)
-    head(res)
-    pdf(paste('_result/',basename(fl),'.pdf',sep=''),width=60,height=40)
+    res = read.table(fl, header=TRUE);
+    head(res);
+    pdf(paste('_result/',basename(fl),'.pdf',sep=''),width=60,height=40);
     # mgp = c(3,4,0):4->xlab(ylab) and x(y) axes distance 
     # mai image margin pdf 
-    par(mai=c(5,5,5,5),mgp=c(3, 3, 0))
+    par(mai=c(5,5,5,5),mgp=c(3, 3, 0));
 
-    if(styl=='fc'){
-        with(res,plot(Frequency,Coverage,pch=19,cex=2,cex.axis=4,main="",xlab="",ylab=""))
-        with(subset(res, repFamilySINE=='Alu' ), points(Frequency,Coverage, pch=19,cex=2,col="red"))
-        mtext(basename(fl),side=3,line=8,cex=6)
-        mtext("Frequency",side=1,line=8,cex=6)
-        mtext("Coverage",side=2,line=8,cex=6)
-    } else if (styl=='-log10csqrtf') {
-        with(res,plot(-log10(Coverage),sqrt(Frequency),pch=19,cex=2,cex.axis=4,main="",xlab="",ylab=""))
-        with(subset(res, repFamilySINE=='Alu' ), points(-log10(Coverage),sqrt(Frequency), pch=19,cex=2,col="red"))
-        mtext(basename(fl),side=3,line=8,cex=6)
-        mtext("-log10(Coverage)",side=1,line=8,cex=6)
-        mtext("sqrt(Frequency)",side=2,line=8,cex=6)
-    } else if (styl=='-log10flog2c'){
-        with(res,plot(-log10(Frequency),log2(Coverage),pch=19,cex=2,cex.axis=4,main="",xlab="",ylab=""))
-        with(subset(res, repFamilySINE=='Alu' ), points(-log10(Frequency),log2(Coverage), pch=19,cex=2,col="red"))
-        mtext(basename(fl),side=3,line=8,cex=6)
-        mtext("-log10(Frequency)",side=1,line=8,cex=6)
-        mtext("log2(Coverage)",side=2,line=8,cex=6)
+    if(mod=='fc'){
+        with(res,plot(Frequency,Coverage,pch=19,cex=2,cex.axis=4,main="",xlab="",ylab=""));
+        with(subset(res, eval(parse(text=opt$condition))), points(Frequency,Coverage, pch=19,cex=2,col="red"));
+        #with(subset(res, repFamilySINE=='Alu' ), points(Frequency,Coverage, pch=19,cex=2,col="red"));
+        #img_main = basename(fl);
+        img_xlab = "Frequency";
+        img_ylab = "Coverage";
+    } else if (mod=='nlog10csqrtf') {
+        with(res,plot(-log10(Coverage),sqrt(Frequency),pch=19,cex=2,cex.axis=4,main="",xlab="",ylab=""));
+        with(subset(res, repFamilySINE=='Alu' ), points(-log10(Coverage),sqrt(Frequency), pch=19,cex=2,col="red"));
+        #img_main = basename(fl);
+        img_xlab = "-log10(Coverage)";
+        img_ylab = "sqrt(Frequency)";
+    } else if (mod=='nlog10flog2c'){
+        with(res,plot(-log10(Frequency),log2(Coverage),pch=19,cex=2,cex.axis=4,main="",xlab="",ylab=""));
+        with(subset(res, repFamilySINE=='Alu' ), points(-log10(Frequency),log2(Coverage), pch=19,cex=2,col="red"));
+        #img_main = basename(fl);
+        img_xlab = "-log10(Frequency)";
+        img_ylab = "log2(Coverage)";
     }
+    
+    img_main = basename(fl);
+    mtext(img_main,side=3,line=8,cex=6);
+    mtext(img_xlab,side=1,line=8,cex=6);
+    mtext(img_ylab,side=2,line=8,cex=6);
 
-    dev.off()
+    dev.off();
 }
