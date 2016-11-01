b14aa38c7aa41557e07be3e63617b6e8e7a90d52
Hongshun Yang <yanghongshun@gmail.com>
2016-11-01 00:03:05 -0400

    fix pdf size

--- tools/volcano.plot.r
+++ tools/volcano.plot.r
@@ -10,13 +10,15 @@ if (length(args)==0) {
     stop("At least one argument must be supplied (input file).n", call.=FALSE)
 }
 
+
 fls <- list.files(args[1],pattern='*.csv',full.names=TRUE,recursive=TRUE)
 
 for (fl in fls){
     res <- read.table(fl, header=TRUE)
     head(res)
-    pdf(paste('_result/',basename(fl),'.pdf',sep=''),width=6,height=4,paper='special')
-    with(res,plot(-log2(Coverage),Frequency,pch=19,cex=0.3,main=fl,cex.main=0.5))
-    with(subset(res, repFamilySINE=='Alu' ), points(-log2(Coverage),Frequency, pch=19,cex=0.3,col="red"))
+    pdf(paste('_result/',basename(fl),'.pdf',sep=''),width=60,height=40)
+    par(mai=c(5,5,5,5))
+    with(res,plot(-log10(Coverage),sqrt(Frequency),pch=19,cex=2,main=basename(fl),cex.main=4,cex.axis=2.5,cex.lab=4))
+    with(subset(res, repFamilySINE=='Alu' ), points(-log10(Coverage),sqrt(Frequency), pch=19,cex=2,col="red"))
     dev.off()
 }
