a24d11d9f2dfc61ca9ec7609417bca7562527595
Hongshun Yang <yanghongshun@gmail.com>
2016-10-31 21:35:58 -0400

    * support batch process * the result file(pdf) placed in tools/_result

--- tools/volcano.plot.r
+++ tools/volcano.plot.r
@@ -1,17 +1,22 @@
 #!/usr/bin/env Rscript
 
-#./volcano.plot.r ../result/10262016/LPS/WT-1/xxx.csv
-#../result/10262016/LPS/WT-1/xxx.csv.pdf
+# support batch 
+#./volcano.plot.r ../result/10262016/
+# _result
 
 args = commandArgs(trailingOnly=TRUE)
 # test if there is at least one argument: if not, return an error
 if (length(args)==0) {
     stop("At least one argument must be supplied (input file).n", call.=FALSE)
-} 
-res <- read.table(args[1], header=TRUE)
-head(res)
-pdf(paste(args[1],'.pdf',sep=''),width=6,height=4,paper='special')
-with(res,plot(-log2(Coverage),Frequency,pch=19,cex=0.3,main=args[1],cex.main=0.5))
-with(subset(res, repFamilySINE=='Alu' ), points(-log2(Coverage),Frequency, pch=19,cex=0.3,col="red"))
-dev.off()
+}
 
+fls <- list.files(args[1],pattern='*.csv',full.names=TRUE,recursive=TRUE)
+
+for (fl in fls){
+    res <- read.table(fl, header=TRUE)
+    head(res)
+    pdf(paste('_result/',basename(fl),'.pdf',sep=''),width=6,height=4,paper='special')
+    with(res,plot(-log2(Coverage),Frequency,pch=19,cex=0.3,main=fl,cex.main=0.5))
+    with(subset(res, repFamilySINE=='Alu' ), points(-log2(Coverage),Frequency, pch=19,cex=0.3,col="red"))
+    dev.off()
+}
